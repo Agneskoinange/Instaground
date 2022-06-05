829c23bf7814db2ce5cc699302f9544cc92820b3
Agnes <koinageagness@gmail.com>
2022-06-05 15:30:31 +0300

    configure email deatils

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,14 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+from decouple import config
+#...... 
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
