d64a2f30d897dc68258f144b6e28a4e69a7becf2
Agnes <koinageagness@gmail.com>
2022-06-07 20:41:49 +0300

    adjust settings.py for deployment

--- project/settings.py
+++ project/settings.py
@@ -142,17 +142,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-
-#...... 
-
-# Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
 # adding config
 cloudinary.config( 
   cloud_name = "dopvdc9a1", 
